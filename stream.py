7b3e9f5fd368a4cd2675fa03c350a944f5f4878c
Dimitri Rudnev <dimitri.rudnev@gmail.com>
2020-05-17 19:53:12 -0700

    Adding emoji extraction , url and media tracking

--- stream.py
+++ stream.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # Import required libraries
-import json , time, datetime
+import json , datetime
+from time import sleep
+import emoji
 from threading import Thread
 from collections import deque
 from collections import Counter
 from twython import TwythonStreamer
-
-#from requests.exceptions import ChunkedEncodingError
-
+from urllib.parse import urlparse
 
 
 # Get Twitter Stream and Queue message to the Queue
@@ -35,49 +35,99 @@ class TweetAnalytics():
         # iteration timer , will reset at every output
         self.iteration_timer = datetime.datetime.now()
 
-        # Tweets of tweets (total )
-        self.total_tweets = 0
+        # Tweet stat counters
+        self.tweets_total = 0
+        self.tweets_with_emoji = 0
+        self.tweets_with_url = 0
+        self.tweets_with_media=0
 
         # Tweets since last output
-        self.iteration_tweets = 0
-        self.emoji_tweets = 0
+        self.tweets_iteration = 0
 
+        # Track Top items in tweets
         self.hashtags = Counter()
         self.emojis = Counter()
+        self.url_domains = Counter()
+
+        self.media_type = Counter()
 
     def add_tweet(self,tweet):
         # Increment
-        self.total_tweets += 1
-        self.iteration_tweets += 1
+        self.tweets_total += 1
+        self.tweets_iteration += 1
 
         self._hashtag_counts(tweet['entities']['hashtags'])
         self._emoji_counts(tweet['text'])
+        if 'media' in tweet['entities'].keys():
+            self._media_counts(tweet['entities']['media'])
+        self._url_counts(tweet['entities']['urls'])
 
     # Increment hashtag counts
     def _hashtag_counts(self, hashtags):
         for hashtag in hashtags:
-            self.hashtags.update({"{}".format(hashtag['text']): 1})
+            self.hashtags.update({ hashtag['text'] : 1})
 
     def _emoji_counts(self,text):
-        pass
+        if emoji.emoji_count(text) > 0:
+            self.tweets_with_emoji += 1
+            emoji_list = emoji.emoji_lis(text)
+            for em in emoji_list:
+                self.emojis.update({ emoji.demojize(em['emoji']) : 1 })
+
+    def _url_counts(self,url_list):
+        if len(url_list) > 0:
+            self.tweets_with_url += 1
+            domain_list = []
+            for url in url_list:
+                #Extract domain only
+                domain_list.append( urlparse(url['expanded_url']).netloc )
+
+            for domain in set(domain_list):
+                self.url_domains.update({ domain : 1 })
+
+
+    def _media_counts(self,media_list):
+        if len(media_list) > 0:
+            self.tweets_with_url += 1
+            media_types=[]
+            for item in media_list:
+                media_types.append(item['type'])
+
+            # Get unique items from the media list
+            # This way each media only reported once per tweet
+            for type in set(media_types):
+                self.media_type.update({ type : 1 })
 
     def output(self):
         current_time = datetime.datetime.now()
 
-        processing_rate_current = int(self.iteration_tweets / int((current_time - self.iteration_timer).total_seconds()))
-        processed_tweets = self.iteration_tweets
-        self.iteration_tweets = 0
+        processing_rate_current = int(self.tweets_iteration / int((current_time - self.iteration_timer).total_seconds()) ) #TODO: avoid division by zero
+        processed_tweets = self.tweets_iteration
+        self.tweets_iteration = 0
         self.iteration_timer = datetime.datetime.now()
-        processing_rate_overall = int(self.total_tweets / int((current_time - self.start_timer).total_seconds()))
+        processing_rate_overall = int( self.tweets_total / int((current_time - self.start_timer).total_seconds()) ) #TODO: avoid division by zero
 
         out = {
             'datetime': current_time.strftime("%Y-%m-%dT%H:%M:%S%z"),
-            'total_tweets': self.total_tweets,
+            'tweets_total': self.tweets_total,
+            'tweets_with_url': self.tweets_with_url,
+            'tweets_with_url_pct': int( 100 * self.tweets_with_url / self.tweets_total if self.tweets_total > 0 else 0 ), # avoid division by zero
+
+            'tweets_with_emoji': self.tweets_with_emoji,
+            'tweets_with_emoji_pct': int( 100 * self.tweets_with_emoji / self.tweets_total if self.tweets_total > 0 else 0 ), # avoid division by zero
+
+            'tweets_with_media': self.tweets_with_media,
+            'tweets_with_media_pct': int( 100 * self.tweets_with_media / self.tweets_total if self.tweets_total > 0 else 0 ), # avoid division by zero
+
             'processing_rate_overall_rps': processing_rate_overall,
-            'tweets_processed': processed_tweets,
+            'tweets_processed_last': processed_tweets,
             'processing_rate_current_rps': processing_rate_current,
+
             'top_emoji': self.emojis.most_common(10),
             'top_hashtags': self.hashtags.most_common(10),
+            'top_urls': self.url_domains.most_common(10),
+            'top_media': self.media_type.most_common(10),
+
         }
         return json.dumps(out)
 
@@ -108,9 +158,8 @@ def stream_tweets(tweets_queue):
 
 def analytics_output(frequency,trends):
     while True:
-        time.sleep(frequency)
+        sleep(frequency)
         print(trends.output())
-
 
 
 if __name__ == '__main__':
